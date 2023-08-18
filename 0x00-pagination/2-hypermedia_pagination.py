b2afb75c224d89923ac87c78f70a375ec408c9fc
Shedrack John <113800368+shedonyii@users.noreply.github.com>
2023-08-18 22:04:21 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -65,3 +65,4 @@ class Server:
             'next_page': page + 1 if page < total_pages else None,
             'prev_page': page - 1 if page != 1 else None,
             'total_pages': total_pages
+        }
